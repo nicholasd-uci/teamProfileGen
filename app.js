7a51df59fa71356632656eb390f18d45226ec925
Nicholas Dallas <nicholas.dallas0@gmail.com>
2020-08-15 17:34:13 -0700

    updated const in lib folder

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// these are being called from the lib
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
@@ -10,7 +11,9 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
+const buildProfile = profile => {
+    
+}
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
